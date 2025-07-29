81a4d80ad93fd773ec683b4737b47e55acc415c4
Rashid Amanzholov <rashid.amanzholov@gmail.com>
2025-07-29 21:04:41 +0200

    Added custom icons

--- lib/features/departures/presentation/widgets/departure_list_item.dart
+++ lib/features/departures/presentation/widgets/departure_list_item.dart
@@ -1,5 +1,6 @@
 import 'package:bvg_departures_app/core/constants/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import '../../data/models/departure.dart';
 
 class DepartureListItem extends StatelessWidget {
@@ -22,13 +23,13 @@ class DepartureListItem extends StatelessWidget {
         ],
       ),
       child: ListTile(
-        leading: _getTransportIcon(departure.line?.mode),
+        leading: _getTransportIcon(departure.line?.product),
         title: Text(
           '${departure.line?.name} to ${departure.direction}',
           style: Theme.of(context).textTheme.bodyLarge,
         ),
         subtitle: Text(
-          'Departs at ${departure.formattedWhen}${departure.delay != 0 ? ' (+${departure.delay}m)' : ''}',
+          'Departs at ${departure.formattedWhen}${departure.delay != 0 || departure.delay != null ? ' (${departure.delay}m)' : ''}',
           style: Theme.of(context).textTheme.bodyMedium,
         ),
         trailing: departure.platform != null
@@ -41,18 +42,48 @@ class DepartureListItem extends StatelessWidget {
     );
   }
 
+  /// Returns an icon based on the transport type.
+  /// Supported types:
+  /// - suburban: S-Bahn
+  /// - subway: U-Bahn
+  /// - tram: Tram
+  /// - bus: Bus
+  /// - ferry: Fähre
+  /// - express: IC/ICE
+  /// - regional: RB/RE
+  /// If the type is not recognized, a placeholder icon is returned.
   Widget _getTransportIcon(String? type) {
+    String iconPath;
     switch (type?.toLowerCase()) {
-      case 'bus':
-        return const Icon(Icons.directions_bus);
+      case 'suburban':
+        iconPath = 'assets/svg/suburban.svg';
+        break;
+      case 'subway':
+        iconPath = 'assets/svg/subway.svg';
+        break;
       case 'tram':
-        return const Icon(Icons.tram);
-      case 'ubahn':
-        return const Icon(Icons.subway);
-      case 'sbahn':
-        return const Icon(Icons.train);
+        iconPath = 'assets/svg/tram.svg';
+        break;
+      case 'bus':
+        iconPath = 'assets/svg/bus.svg';
+        break;
+      case 'ferry':
+        iconPath = 'assets/svg/ferry.svg';
+        break;
+      case 'express':
+        iconPath = 'assets/svg/express.svg';
+        break;
+      case 'regional':
+        iconPath = 'assets/svg/regional.svg';
+        break;
       default:
-        return const Icon(Icons.directions);
+        iconPath = 'assets/svg/placeholder.svg';
     }
+    return SvgPicture.asset(
+      iconPath,
+      width: 48.0,
+      height: 48.0,
+      // colorFilter: const ColorFilter.mode(AppColors.primary, BlendMode.srcIn),
+    );
   }
 }
